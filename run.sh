ed826534709d88c0ec7729d9b2c66e2be473a73b
NK <namkyo@outlook.jp>
2024-04-05 22:03:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,39 @@
 #!/bin/sh
-cd
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+DIR=$PWD # get current dir
+# if current dir is /root print ~
+if [ "$PWD" = "/root"* ]; then
+    DIR="~${PWD#/root}"
+fi
+run_cmd() {
+    read -p "root@wp:$DIR# " CMD
+    eval "$CMD"
+
+    # Update DIR after executing command
+    DIR=$PWD
+    if [ "$PWD" = "/root"* ]; then
+        DIR="~${PWD#/root}"
+    fi
+    
+    printf "root@wp:$DIR# \n"
+    run_user_cmd
+}
+
+run_user_cmd() {
+    read -p "user@wp:$DIR# " CMD2
+    eval "$CMD2"
+
+    # Update DIR after executing command
+    DIR=$PWD
+    if [ "$PWD" = "/root"* ]; then
+        DIR="~${PWD#/root}"
+    fi
+    
+    printf "root@wp:$DIR# \n"
+    run_cmd
+}
+
+run_cmd
